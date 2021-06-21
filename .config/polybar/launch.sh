6c382ba000efeccec387060651b807a9fc2113de
Antoine Lelaisant <antoine.lelaisant@knplabs.com>
2021-06-21 09:37:44 +0200

    fix: polybar launch for multi screens

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,20 +4,21 @@ dir="$HOME/.config/polybar"
 themes=(`ls --hide="launch.sh" $dir`)
 
 launch_bar() {
-	# Terminate already running bar instances
 	killall -q polybar
-
-	# Wait until the processes have been shut down
 	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-	# Launch the bar
 	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
 		polybar -q top -c "$dir/$style/config.ini" &
 		polybar -q bottom -c "$dir/$style/config.ini" &
+
 	elif [[ "$style" == "pwidgets" ]]; then
 		bash "$dir"/pwidgets/launch.sh --main
+
 	else
-		polybar -q main -c "$dir/$style/config.ini" &	
+		#👇 👉 launching multiple monitors --> make sure to add monitor = ${env:MONITOR:} in the config
+    for m in $(bspc query --monitors --names); do
+      MONITOR=$m polybar -q main -c "$dir/$style/config.ini" &
+    done
 	fi
 }
 
