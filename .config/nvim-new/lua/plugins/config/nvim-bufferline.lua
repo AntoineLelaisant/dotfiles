f534589fb4ea4d8f59316aa4c42d31a973280273
Antoine Lelaisant <antoine.lelaisant@knplabs.com>
2021-07-27 19:07:39 +0200

    fix: nvim-bufferline

--- .config/nvim-new/lua/plugins/config/nvim-bufferline.lua
+++ .config/nvim-new/lua/plugins/config/nvim-bufferline.lua
@@ -1,27 +1,4 @@
 require'bufferline'.setup{
-  highlights = {
-    fill = {
-      guifg = '#F4CEA9',
-      guibg = "#3A3A3A"
-    },
-    background = {
-      guifg = '#F4CEA9',
-      guibg = "#3A3A3A"
-    },
-    close_button_selected = {
-      guifg = '#F4CEA9',
-      guibg = "#3A3A3A"
-    },
-    buffer_selected = {
-      guifg = '#F4CEA9',
-      guibg = "#3A3A3A",
-      gui = "bold,italic"
-    },
-    separator_selected = {
-        guifg = '#F4CEA9',
-        guibg = '#3A3A3A'
-    }
-  },
   options = {
     separator_style = 'thin',
   }
