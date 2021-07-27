e7da566165a6d7a6c4a7bebd201e9d85ce85a6e3
Antoine Lelaisant <antoine.lelaisant@knplabs.com>
2021-07-27 22:01:21 +0200

    fix: nvim tree configuration

--- .config/nvim-new/lua/plugins/config/nvim-tree.lua
+++ .config/nvim-new/lua/plugins/config/nvim-tree.lua
@@ -2,5 +2,13 @@ vim.g.nvim_tree_ignore = {'.git', 'node_modules'}
 vim.g.nvim_tree_gitignore = 1
 vim.g.nvim_tree_update_cwd = 1
 vim.g.nvim_tree_disable_netrw = 1
+vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_auto_open = 0
 vim.g.nvim_tree_auto_close = 0
+vim.g.nvim_tree_lsp_diagnostics = 1
+vim.g.nvim_tree_show_icons = {
+  git = 1,
+  folders = 1,
+  files = 1,
+  folder_arrows = 0,
+}
